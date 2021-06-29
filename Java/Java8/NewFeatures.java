d6d4cf0f8e00ecc89a8c3ff5c5e6142e00a5b44b
bhavnavarshney <bhavna.varshney25@gmail.com>
2021-06-29 23:40:41 +0530

    Adding java 8 new features related to interfaces

---
+++ Java/Java8/NewFeatures.java
@@ -0,0 +1,12 @@
+package Java8;
+
+public class NewFeatures {
+    public static void main(String[] args) {
+        Interfaces8 i1 = new Interfaces8();
+        i1.act();
+
+        defaultInterfaceEg i2 = new defaultInterfaceEg();
+        i2.act();
+    }
+
+}
